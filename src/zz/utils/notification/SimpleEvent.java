dcc59c5e26d0312ccb93fd9c73a9962555430ade
gpothier <gpothier@9d7561e4-2ed5-0310-851a-80adb45e5f51>
2005-03-31 20:49:18 +0000

    Added convenience event linked to property Improved AsyncThumbnailCache Created ProgressModel

--- src/zz/utils/notification/SimpleEvent.java
+++ src/zz/utils/notification/SimpleEvent.java
@@ -3,8 +3,12 @@
  */
 package zz.utils.notification;
 
+import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
 import java.util.List;
+
+import zz.utils.properties.IProperty;
+import zz.utils.properties.IPropertyListener;
 
 /**
  * A basic implementation of {@link zz.utils.notification.IEvent}
@@ -37,4 +41,34 @@ public class SimpleEvent<T> extends AbstractEvent<T>
 		}
 	}
 
+	/**
+	 * Returns an event object that is fired whenever the specified property changes.
+	 */
+	public static <T> SimpleEvent<T> createEvent (IProperty<T> aProperty)
+	{
+		return new PropertyChangeEvent<T>(aProperty);
+	}
+
+	/**
+	 * This class represents an event this is fired whenever a given property changes.
+	 * @author gpothier
+	 */
+	private static class PropertyChangeEvent<T> extends SimpleEvent<T>
+	implements IPropertyListener<T>
+	{
+		public PropertyChangeEvent(IProperty<T> aProperty)
+		{
+			aProperty.addListener(this);
+		}
+
+		public void propertyChanged(IProperty<T> aProperty, T aOldValue, T aNewValue)
+		{
+			fire(aNewValue);
+		}
+
+		public void propertyValueChanged(IProperty<T> aProperty)
+		{
+			fire(aProperty.get());
+		}
+	}
 }
--- src/zz/utils/ui/thumbnail/AsyncThumbnailCache.java
+++ src/zz/utils/ui/thumbnail/AsyncThumbnailCache.java
@@ -14,9 +14,10 @@ import java.util.concurrent.LinkedBlockingQueue;
 import zz.utils.ui.thumbnail.ThumbnailCache.Key;
 
 /**
- * A thumbnail cache that always responds immediately to requests.
- * A separate thread creates the thumbnails in the background.
- * Listeners can regigister themselves to be notified when new thumbnails have been created.
+ * A thumbnail cache that always responds immediately to requests. A separate
+ * thread creates the thumbnails in the background. Listeners can regigister
+ * themselves to be notified when new thumbnails have been created.
+ * 
  * @author gpothier
  */
 public abstract class AsyncThumbnailCache<T> extends ThumbnailCache<T> implements Runnable
@@ -30,39 +31,46 @@ public abstract class AsyncThumbnailCache<T> extends ThumbnailCache<T> implement
 		{
 			return AsyncThumbnailCache.this.createThumbnail(null, aMaxSize);
 		}
-		
 	};
-	
+
 	private BlockingQueue<Key<T>> itsLoadQueue = new LinkedBlockingQueue<Key<T>>();
+
 	private Set<Key<T>> itsQueuedKeys = new HashSet<Key<T>>();
-	
+
+	/**
+	 * We keep a set of ignored keys so that we don't try to indefinitely reload
+	 * keys that can't be loaded.
+	 */
+	private Set<Key<T>> itsIgnoredKeys = new HashSet<Key<T>>();
+
 	private List<IAsyncThumbnailCacheListener> itsListeners = new ArrayList<IAsyncThumbnailCacheListener>();
 
 	public AsyncThumbnailCache()
 	{
 		new Thread(this).start();
 	}
-	
+
 	protected BufferedImage getThumbnail(Key<T> aKey)
 	{
 		BufferedImage theImage = getCached(aKey);
-		
-		if (theImage == null) 
+
+		if (theImage == null)
 		{
 			queueLoading(aKey);
 			return getLIPImage(aKey.getSize());
 		}
 		else return theImage;
 	}
-	
+
 	/**
 	 * Enqueues q request for loading the image described by the given key.
 	 */
-	private void queueLoading (Key<T> aKey)
+	private void queueLoading(Key<T> aKey)
 	{
+        if (itsIgnoredKeys.contains(aKey)) return;
 		if (itsQueuedKeys.add(aKey)) itsLoadQueue.add(aKey);
 	}
-	
+
 	public void run()
 	{
 		try
@@ -71,10 +79,22 @@ public abstract class AsyncThumbnailCache<T> extends ThumbnailCache<T> implement
 			{
 				Key<T> theKey = itsLoadQueue.take();
 				itsQueuedKeys.remove(theKey);
-				
-				BufferedImage theImage = createThumbnail(theKey);
-				cache(theKey, theImage);
-				fireThumbnailCreated();
+
+				try
+				{
+					BufferedImage theImage = createThumbnail(theKey);
+                    if (theImage == null) itsIgnoredKeys.add(theKey);
+                    else
+                    {
+						cache(theKey, theImage);
+						fireThumbnailCreated();
+                    }
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+                    itsIgnoredKeys.add(theKey);
+				}
 			}
 		}
 		catch (InterruptedException e)
@@ -82,22 +102,22 @@ public abstract class AsyncThumbnailCache<T> extends ThumbnailCache<T> implement
 			e.printStackTrace();
 		}
 	}
-	
-	private BufferedImage getLIPImage (int aSize)
+
+	private BufferedImage getLIPImage(int aSize)
 	{
 		return itsLIPCache.getThumbnail(aSize, aSize);
 	}
-	
-	public void addListsner (IAsyncThumbnailCacheListener aListener)
+
+	public void addListsner(IAsyncThumbnailCacheListener aListener)
 	{
 		itsListeners.add(aListener);
 	}
 
-	public void removeListsner (IAsyncThumbnailCacheListener aListener)
+	public void removeListsner(IAsyncThumbnailCacheListener aListener)
 	{
 		itsListeners.remove(aListener);
 	}
-	
+
 	protected void fireThumbnailCreated()
 	{
 		for (IAsyncThumbnailCacheListener theListener : itsListeners)
