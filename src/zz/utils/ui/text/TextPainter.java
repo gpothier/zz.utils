15e0ad10417a3c00a8be021f70bafcb558bf53ab
gpothier <gpothier@9d7561e4-2ed5-0310-851a-80adb45e5f51>
2005-03-31 22:48:47 +0000

    Improved TextPainter

--- src/zz/utils/ui/text/TextPainter.java
+++ src/zz/utils/ui/text/TextPainter.java
@@ -16,6 +16,8 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.text.AttributedCharacterIterator;
 import java.text.AttributedString;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Provides utilities to paint text.
@@ -24,6 +26,37 @@ import java.text.AttributedString;
  */
 public class TextPainter
 {
+	public static enum VerticalAlignment {
+		TOP, CENTER, BOTTOM
+	}
+	
+	public static enum HorizontalAlignment {
+		LEFT, CENTER, RIGHT
+	}
+
+	public static void paint(
+			Graphics2D aGraphics, 
+			Font aFont,
+			boolean aUnderline,
+			Paint aColor,
+			String aText,
+			Shape aShape,
+			VerticalAlignment aVAlign,
+			HorizontalAlignment aHAlign)
+	{
+		if (aText == null || aText.length() == 0) return;
+		
+		AttributedString theString = new AttributedString(aText);
+		
+		theString.addAttribute(TextAttribute.FOREGROUND, aColor);
+		theString.addAttribute(TextAttribute.FONT, aFont);
+		theString.addAttribute(
+				TextAttribute.UNDERLINE, 
+				aUnderline ? TextAttribute.UNDERLINE_ON : null);
+		
+		paint (aGraphics, theString, aShape, aVAlign, aHAlign);
+	}
+	
 	/**
 	 * Paints a text in the specified shape.
 	 * 
@@ -33,11 +66,10 @@ public class TextPainter
 	 */
 	public static void paint(
 			Graphics2D aGraphics, 
-			Font aFont,
-			boolean aUnderline,
-			Paint aPaint,
-			String aText, 
-			Shape aShape)
+			AttributedString aText, 
+			Shape aShape,
+			VerticalAlignment aVAlign,
+			HorizontalAlignment aHAlign)
 	{
 		Rectangle2D theBounds;
 		
@@ -59,56 +91,71 @@ public class TextPainter
 		}
 		else theBounds = aShape.getBounds2D();
 
-		paint(aGraphics, aFont, aUnderline, aPaint, aText, theBounds);
+		paint(aGraphics, aText, theBounds, aVAlign, aHAlign);
 	}
 
 	public static void paint(
 			Graphics2D aGraphics, 
-			Font aFont, 
-			boolean aUnderline,
-			Paint aPaint,
-			String aText, 
-			Rectangle2D aBounds)
+			AttributedString aText, 
+			Rectangle2D aBounds,
+			VerticalAlignment aVAlign,
+			HorizontalAlignment aHAlign)
 	{
-		if (aText.length() == 0) return;
+		if (aText == null) return;
 		
-		//Save previous font & set requested font
-		Font thePreviousFont = aGraphics.getFont();
-		if (aFont != null) aGraphics.setFont(aFont);
-		if (aPaint != null) aGraphics.setPaint(aPaint);
+		// Save previous clip & set new clip
+		Shape thePreviousClip = aGraphics.getClip();
+		aGraphics.clip(aBounds);
 		
 		FontRenderContext theContext = aGraphics.getFontRenderContext();
 
 		float theX = (float) aBounds.getX();
-		float theY = (float) aBounds.getY();
 		float theW = (float) aBounds.getWidth();
-		float theMaxY = (float) aBounds.getMaxY();
+		float theH = (float) aBounds.getHeight();
+				
+		AttributedCharacterIterator theIterator = aText.getIterator();
 
-		AttributedString theString = new AttributedString(aText);
-		if (aFont != null) 
+		LineBreakMeasurer theMeasurer = new LineBreakMeasurer(theIterator, theContext);
+
+		List <TextLayout>  theListLayout=new ArrayList<TextLayout>();  
+		
+		float theY = 0;
+		while (theMeasurer.getPosition() < theIterator.getEndIndex() && theY < theH)
 		{
-			theString.addAttribute(TextAttribute.FONT, aFont);
-			theString.addAttribute(
-					TextAttribute.UNDERLINE, 
-					aUnderline ? TextAttribute.UNDERLINE_ON : null);
+			TextLayout theLayout = theMeasurer.nextLayout(theW);
+			theY = theY + theLayout.getAscent()+theLayout.getDescent() +theLayout.getLeading();
+			theListLayout.add(theLayout);
 		}
 		
-		AttributedCharacterIterator theIterator = theString.getIterator();
-
-		LineBreakMeasurer measurer = new LineBreakMeasurer(theIterator, theContext);
-
-		while (measurer.getPosition() < aText.length() && theY < theMaxY)
+		
+		float theYMargin = 0;
+		
+		switch (aVAlign)
 		{
-			TextLayout layout = measurer.nextLayout(theW);
-
-			theY += layout.getAscent();
-			float theDX = layout.isLeftToRight() ? 0 : (theW - layout.getAdvance());
-
-			layout.draw(aGraphics, theX + theDX, theY);
-			theY += layout.getDescent() + layout.getLeading();
+			case TOP: theYMargin = 0;break;
+			case CENTER:  theYMargin = (theH-theY)/2f; break;
+			case BOTTOM:  theYMargin = theH-theY;break;	
 		}
 		
-		// restore previous font
-		aGraphics.setFont(thePreviousFont);
+		theY = (float) aBounds.getY() + theYMargin;
+		for(TextLayout theLayout : theListLayout)
+		{
+			theY += theLayout.getAscent();
+			float theLineW = (float) theLayout.getBounds().getWidth();
+			
+			float theXMargin = 0;
+			switch (aHAlign)
+			{
+				case LEFT: theXMargin = 0;break;
+				case CENTER: theXMargin = (theW - theLineW)/2f;break;
+				case RIGHT:	theXMargin = theW - theLineW;break;
+			}
+			
+			theLayout.draw(aGraphics, theX + theXMargin, theY);
+			theY += theLayout.getDescent() +theLayout.getLeading();
+		}
+		
+		// restore previous clip
+		aGraphics.setClip(thePreviousClip);
 	}
 }
