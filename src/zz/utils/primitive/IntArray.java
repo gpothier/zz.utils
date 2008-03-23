ab3416c18a35c8896a1902d559e544ecf8346a02
gpothier <gpothier@9d7561e4-2ed5-0310-851a-80adb45e5f51>
2008-03-23 16:24:47 +0000

    stuff with primitives

--- src/zz/utils/primitive/IntArray.java
+++ src/zz/utils/primitive/IntArray.java
@@ -3,6 +3,10 @@
  */
 package zz.utils.primitive;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * A primitive int array that grows as needed
  * @author gpothier
@@ -10,6 +14,7 @@ package zz.utils.primitive;
 public class IntArray
 {
 	private int[] itsData;
+	private int itsSize;
 
 	public IntArray()
 	{
@@ -23,13 +28,34 @@ public class IntArray
 	
 	public int get(int aIndex)
 	{
-		return aIndex < itsData.length ? itsData[aIndex] : 0;
+		return aIndex < itsSize ? itsData[aIndex] : 0;
+	}
+	
+	public int size()
+	{
+		return itsSize;
+	}
+	
+	public boolean isEmpty()
+	{
+		return itsSize == 0;
+	}
+	
+	protected void setSize(int aSize)
+	{
+		itsSize = aSize;
 	}
 	
 	public void set(int aIndex, int aValue)
 	{
 		ensureSize(aIndex+1);
 		itsData[aIndex] = aValue;
+		itsSize = Math.max(itsSize, aIndex+1);
+	}
+	
+	public void clear()
+	{
+		itsSize = 0;
 	}
 	
 	private void ensureSize(int aSize)
@@ -41,5 +67,30 @@ public class IntArray
 		System.arraycopy(itsData, 0, theNewData, 0, itsData.length);
 		itsData = theNewData;
 	}
+
+	public int[] toArray()
+	{
+		int[] theResult = new int[size()];
+		System.arraycopy(itsData, 0, theResult, 0, size());
+		return theResult;
+	}
 	
+	/**
+	 * Transforms a collection of {@link Integer}s to an array of native ints.
+	 */
+	public static int[] toIntArray(Collection<Integer> aCollection)
+	{
+		int[] theResult = new int[aCollection.size()];
+		int i=0;
+		for(Integer theInt : aCollection) theResult[i++] = theInt;
+		return theResult;
+	}
+	
+	public static List<Integer> toList(int[] aArray)
+	{
+		if (aArray == null) return null;
+		List<Integer> theList = new ArrayList<Integer>();
+		for(int i : aArray) theList.add(i);
+		return theList;
+	}
 }
