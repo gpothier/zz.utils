7521d11859ab94917247e658b8bfc55586b27e32
omotelet <omotelet@9d7561e4-2ed5-0310-851a-80adb45e5f51>
2008-04-18 13:08:29 +0000

    Partch for a GUI problem need to be reviewed.

--- src/zz/utils/cache/MRUBuffer.java
+++ src/zz/utils/cache/MRUBuffer.java
@@ -10,28 +10,28 @@ import zz.utils.list.NakedLinkedList;
 import zz.utils.list.NakedLinkedList.Entry;
 
 /**
- * A buffer that keeps the most recently used items in memory.
- * Subclasses can be notified of dropped items by overriding
- * the {@link #dropped(Object)} method.
+ * A buffer that keeps the most recently used items in memory. Subclasses can be
+ * notified of dropped items by overriding the {@link #dropped(Object)} method.
+ * 
  * @author gpothier
  */
 public abstract class MRUBuffer<K, V>
 {
 	private int itsCacheSize;
-	
+
 	private Map<K, Entry<V>> itsCache;
-	
+
 	/**
 	 * Most recently used items list
 	 */
 	private NakedLinkedList<V> itsItemsList = new NakedLinkedList<V>();
-	
+
 	public MRUBuffer(int aCacheSize, boolean aUseMap)
 	{
 		itsCacheSize = aCacheSize;
 		itsCache = aUseMap ? new HashMap<K, Entry<V>>() : null;
 	}
-	
+
 	public MRUBuffer(int aCacheSize)
 	{
 		this(aCacheSize, true);
@@ -41,28 +41,28 @@ public abstract class MRUBuffer<K, V>
 	 * Returns the key that corresponds to the given value.
 	 */
 	protected abstract K getKey(V aValue);
-	
+
 	private Thread itsThread = null;
-	
-//	private void checkThread()
-//	{
-//		Thread theCurrentThread = Thread.currentThread();
-//		if (itsThread != theCurrentThread) 
-//		{
-//			System.out.println("MRUBuffer.checkThread()");
-//			itsThread = theCurrentThread;
-//		}
-//	}
-//	
+
+	// private void checkThread()
+	// {
+	// Thread theCurrentThread = Thread.currentThread();
+	// if (itsThread != theCurrentThread)
+	// {
+	// System.out.println("MRUBuffer.checkThread()");
+	// itsThread = theCurrentThread;
+	// }
+	// }
+	//	
 	/**
-	 * Brings the specified entry to the top of the buffer.
-	 * This is done automatically when an entry is retrieved or added.
+	 * Brings the specified entry to the top of the buffer. This is done
+	 * automatically when an entry is retrieved or added.
 	 */
 	public void use(Entry<V> aEntry)
 	{
-//		checkThread();
-		
-		if (aEntry.isAttached()) 
+		// checkThread();
+
+		if (aEntry.isAttached())
 		{
 			itsItemsList.moveLast(aEntry);
 		}
@@ -74,43 +74,42 @@ public abstract class MRUBuffer<K, V>
 			{
 				Entry<V> theFirst = itsItemsList.getFirstEntry();
 				itsItemsList.remove(theFirst);
-				
+
 				V theValue = theFirst.getValue();
-				if (itsCache != null) itsCache.remove(getKey(theValue));
+				if (itsCache != null && theValue != null) itsCache.remove(getKey(theValue));
 				dropped(theValue);
-			}			
+			}
 		}
 	}
 
 	/**
-	 * Determines if the oldest element should be dropped, given the current number
-	 * of cached items.
+	 * Determines if the oldest element should be dropped, given the current
+	 * number of cached items.
 	 */
 	protected boolean shouldDrop(int aCachedItems)
 	{
 		return aCachedItems >= itsCacheSize;
 	}
-	
+
 	/**
-	 * This method is called when an item is dropped. 
-	 * This method does nothing by default.
+	 * This method is called when an item is dropped. This method does nothing
+	 * by default.
 	 */
 	protected void dropped(V aValue)
 	{
 	}
 
 	/**
-	 * Called when a value that was not previously in the cache
-	 * is now included in thecache.
-	 * This method does nothing by default.
+	 * Called when a value that was not previously in the cache is now included
+	 * in thecache. This method does nothing by default.
 	 */
 	protected void added(V aValue)
 	{
 	}
-	
+
 	/**
-	 * Retrieves an item from this buffer. If the item is not in
-	 * the buffer, it is fetched with the {@link #fetch(Object)} method.
+	 * Retrieves an item from this buffer. If the item is not in the buffer, it
+	 * is fetched with the {@link #fetch(Object)} method.
 	 */
 	public V get(K aKey)
 	{
@@ -118,8 +117,8 @@ public abstract class MRUBuffer<K, V>
 	}
 
 	/**
-	 * Retrieves an item from this buffer. If the item is not in the buffer
-	 * and aFetch is true, then the item is fetched with the {@link #fetch(Object)}
+	 * Retrieves an item from this buffer. If the item is not in the buffer and
+	 * aFetch is true, then the item is fetched with the {@link #fetch(Object)}
 	 * method. Otherwise, the method returns null.
 	 */
 	public V get(K aKey, boolean aFetch)
@@ -127,17 +126,17 @@ public abstract class MRUBuffer<K, V>
 		Entry<V> theEntry = getEntry(aKey, aFetch);
 		return theEntry != null ? theEntry.getValue() : null;
 	}
-	
+
 	public Entry<V> getEntry(K aKey)
 	{
 		return getEntry(aKey, true);
 	}
 
-	
 	public Entry<V> getEntry(K aKey, boolean aFetch)
 	{
-//		checkThread();
-		if (itsCache == null) throw new UnsupportedOperationException("This MRU buffer does not have a map"); 
+		// checkThread();
+		if (itsCache == null) throw new UnsupportedOperationException(
+				"This MRU buffer does not have a map");
 		Entry<V> theEntry = itsCache.get(aKey);
 		if (theEntry == null && aFetch)
 		{
@@ -146,28 +145,28 @@ public abstract class MRUBuffer<K, V>
 			itsCache.put(aKey, theEntry);
 			added(theValue);
 		}
-		
+
 		if (theEntry != null) use(theEntry);
-		
+
 		return theEntry;
 	}
-	
+
 	/**
-	 * Removes the mapping corresponding to the given key
-	 * from this buffer
+	 * Removes the mapping corresponding to the given key from this buffer
 	 */
 	public void drop(K aKey)
 	{
-//		checkThread();
-		if (itsCache == null) throw new UnsupportedOperationException("This MRU buffer does not have a map"); 
+		// checkThread();
+		if (itsCache == null) throw new UnsupportedOperationException(
+				"This MRU buffer does not have a map");
 		Entry<V> theEntry = itsCache.remove(aKey);
-		if (theEntry != null) 
+		if (theEntry != null)
 		{
 			itsItemsList.remove(theEntry);
 			dropped(theEntry.getValue());
 		}
 	}
-	
+
 	/**
 	 * Drop all mappings from this buffer.
 	 */
@@ -181,22 +180,23 @@ public abstract class MRUBuffer<K, V>
 			dropped(theFirstEntry.getValue());
 		}
 	}
-	
+
 	/**
 	 * Forcefully adds an item to this cache
+	 * 
 	 * @return The newly created entry.
 	 */
 	public Entry<V> add(V aValue)
 	{
-//		checkThread();
+		// checkThread();
 		Entry<V> theEntry = new Entry<V>(aValue);
 		if (itsCache != null) itsCache.put(getKey(aValue), theEntry);
 		added(aValue);
 		use(theEntry);
-		
+
 		return theEntry;
 	}
-	
+
 	/**
 	 * Fetches the value identified by the given key.
 	 */
